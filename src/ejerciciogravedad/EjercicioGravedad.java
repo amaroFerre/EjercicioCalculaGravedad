3ad1b0b8afdc6c84911bbf40fb30bf6fa89f83db
Amarico <Amarico@equipo-zalman>
2018-09-07 10:17:28 +0200

    Realiza un programa que calcule el tiempo que tardará en caer un objeto desde una altura h.

---
+++ src/ejerciciogravedad/EjercicioGravedad.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogravedad;
+
+/**
+ * Realiza un programa que calcule el tiempo que tardará en caer un objeto desde
+ * una altura h. Aplica la fórmula t = √2h g siendo g = 9.81m/s 2
+ *
+ * @author Amarico
+ */
+public class EjercicioGravedad {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
